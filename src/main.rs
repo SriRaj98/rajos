ada150e667d8a825a38a8355c7df5c3b7a8a2ae4
SriRaj98 <raj.sri1998@gmail.com>
2025-11-23 21:52:22 -0800

    Basic binary for thumb target

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+// Disable the standard library
+#![no_std]
+#![no_main] // Disable the standard main entry point
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// Define the entry point for the OS kernel
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
